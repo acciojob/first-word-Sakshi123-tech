a1d5e44097cea4ca6a6a28105f290a643a404d95
Sakshi123-tech <sakshiagnihotri292@gmail.com>
2025-03-17 22:18:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    // Trim leading and trailing spaces
+    str = str.trim();
+    
+    // If the string is empty, return an empty string
+    if (str === '') {
+        return '';
+    }
+    
+    // Find the index of the first space
+    const spaceIndex = str.indexOf(' ');
+
+    // If there's no space, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+    // Return the substring from the start of the string up to the first space
+    return str.substring(0, spaceIndex);
 }
 
 // Do not change the code below
